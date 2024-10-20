b4ca0fc6c6fe93ecae556ad8984c6eff25e923a9
Dima <dima1999ostapenko@gmail.com>
2024-10-20 12:35:57 +0300

    add

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,31 @@
+export const SearchBar = ({ onSearch }) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const form = evt.target;
+    const topic = form.elements.topic.value;
+    if (form.elements.topic.value.trim() === "") {
+      alert("Please enter search term!");
+      return;
+    }
+    onSearch(topic);
+    form.reset();
+  };
+
+  return (
+    <div>
+      <header>
+        <form>
+          <input
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+          <button type="submit">Search</button>
+        </form>
+      </header>
+    </div>
+  );
+};
+
+export default SearchBar;
